881a5ba3044eaeddb1624cc595aa1bc306afe751
tahasan2711 <155311321+tahasan2711@users.noreply.github.com>
2024-07-19 12:15:26 +0000

    endl_vs_n

--- src/Ch01/01_02e/CodeDemo.cpp
+++ src/Ch01/01_02e/CodeDemo.cpp
@@ -2,10 +2,14 @@
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
 
-#include <iostream>
+#include <iostream> //preprocessor directives
 
 int main(){
     std::cout << "Hi There!" << std::endl;
+    // '::' = scope resolution operator
+    // cout is std object
+    // '<<' left-bound insertion operator
+    // 'endl' insert newline by flushing the buffer
 
     std::cout << std::endl << std::endl;
     return (0);
